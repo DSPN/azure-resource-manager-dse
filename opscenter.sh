014dfb2424bcf06aeae5f05ace35a940f086182c
Ben Lackey <blackey-MBP15@Bens-MacBook-Pro.local>
2015-06-28 17:30:03 -0700

    moved license to central file

--- opscenter.sh
+++ opscenter.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
 
-# The MIT License (MIT)
-#
-# Copyright (c) 2015 Microsoft Azure
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-# Script Name: opscenter.sh
-# Author: Trent Swanson - Full Scale 180 Inc github:(trentmswanson)
-# Version: 0.1
-# Last Modified By: Trent Swanson
 # Description: This script installs and configures DataStax OpsCenter and deploys a cluster
 # Parameters:
 #  1 - n: Cluster name
@@ -289,3 +263,4 @@ curl -k -H "opscenter-session: $AUTH_SESSION" -H "Accept: application/json" -X P
 curl -k -H "opscenter-session: $AUTH_SESSION" -H "Accept: application/json" -d "{\"password\": \"$OPS_CENTER_ADMIN_PASS\", \"role\": \"admin\" }" -X PUT https://127.0.0.1:8443/users/admin
 
 exit 0
+
