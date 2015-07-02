fbcfd866345840923f2bb205fd94509ec6dbe983
benofben <ben.lackey@outlook.com>
2015-07-01 22:58:45 -0700

    remove duplicate license

--- vm-disk-utils-0.1.sh
+++ vm-disk-utils-0.1.sh
@@ -1,41 +1,14 @@
 #!/bin/bash
 
-# The MIT License (MIT)
-#
-# Copyright (c) 2015 Microsoft Azure
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-# Script Name: vm-disk-utils.sh
-# Author: Trent Swanson - Full Scale 180 Inc github:(trentmswanson)
-# Version: 0.1
-# Last Modified By:       Trent Swanson
 # Description:
 #  This script automates the partitioning and formatting of data disks
 #  Data disks can be partitioned and formatted as seperate disks or in a RAID0 configuration
 #  The scrtip will scan for unpartitioined and unformatted data disks and partition, format, and add fstab entries
-# Parameters :
+# Parameters:
 #  1 - b: The base directory for mount points (default: /datadisks)
 #  2 - s  Create a striped RAID0 Array (No redundancy)
 #  3 - h  Help 
-# Note : 
-# This script has only been tested on Ubuntu 12.04 LTS and must be root
+# Note: This script has only been tested on Ubuntu 12.04 LTS and must be root
 
 help()
 {
