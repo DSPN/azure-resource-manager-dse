82e57fb87dc8a27fa194a4e78931d7a380017646
benofben <ben.lackey@outlook.com>
2015-07-21 11:55:32 -0700

    OpsCenter json update

--- dsenode.sh
+++ dsenode.sh
@@ -37,13 +37,6 @@ done
 echo "Partitioning and formatting all attached data disks"
 bash vm-disk-utils-0.1.sh
 
-echo "Installing Java"
-add-apt-repository -y ppa:webupd8team/java
-apt-get -y update 
-echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-apt-get -y install oracle-java8-installer
-
 echo "Modifying permissions"
 chmod 777 /mnt
 chmod 777 /datadisks
--- opscenter.sh
+++ opscenter.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# This script installs Oracle Java and DataStax OpsCenter.  It then deploys a DataStax Enterprise cluster using OpsCenter.
+# This script installs DataStax OpsCenter.  It then deploys a DataStax Enterprise cluster using OpsCenter.
 
-echo "Setting default parameters"
-CLUSTER_NAME="Test Cluster"
-DSE_VERSION="4.7.0"
+DSE_VERSION="4.7.1"
 
 while getopts ":n:u:p:e:v:c:U:P:" opt; do
   echo "Option $opt set with value $OPTARG"
@@ -60,13 +58,6 @@ do
         echo "10.0.$i.$(expr $j + 6)  dc${i}vm${j}" >> /etc/hosts
     done
 done
-
-echo "Installing Java"
-add-apt-repository -y ppa:webupd8team/java
-apt-get -y update 
-echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-apt-get -y install oracle-java8-installer
  
 echo "Installing OpsCenter"
 echo "deb http://debian.datastax.com/community stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.community.list
@@ -131,70 +122,148 @@ ACCEPTED_FINGERPRINTS=$(get_node_fingerprints "$NODE_IP_LIST")
 # Create node provisioning document
 sudo tee provision.json > /dev/null <<EOF
 {
-  "cassandra_config": {
-    "authenticator": "org.apache.cassandra.auth.AllowAllAuthenticator",
-    "auto_bootstrap": false,
-    "auto_snapshot": true,
-    "start_native_transport": true,
-    "cluster_name": "${CLUSTER_NAME}",
-    "column_index_size_in_kb": 64,
-    "commitlog_directory": "/mnt/cassandra/commitlog",
-    "commitlog_sync": "periodic",
-    "commitlog_sync_period_in_ms": 10000,
-    "compaction_throughput_mb_per_sec": 16,
-    "concurrent_reads": 32,
-    "concurrent_writes": 32,
-    "data_file_directories": [
-      "/mnt/cassandra/data"
-    ],
-    "dynamic_snitch_badness_threshold": 0.1,
-    "dynamic_snitch_reset_interval_in_ms": 600000,
-    "dynamic_snitch_update_interval_in_ms": 100,
-    "server_encryption_options": {
-      "internode_encryption": "none",
-      "keystore": "conf/.keystore",
-      "keystore_password": "cassandra",
-      "truststore": "conf/.truststore",
-      "truststore_password": "cassandra"
-    },
-    "endpoint_snitch": "com.datastax.bdp.snitch.DseSimpleSnitch",
-    "hinted_handoff_enabled": true,
-    "incremental_backups": false,
-    "index_interval": 128,
-    "initial_token": null,
-    "key_cache_save_period": 14400,
-    "key_cache_size_in_mb": null,
-    "max_hint_window_in_ms": 3600000,
-    "partitioner": "org.apache.cassandra.dht.RandomPartitioner",
-    "request_scheduler": "org.apache.cassandra.scheduler.NoScheduler",
-    "row_cache_save_period": 0,
-    "row_cache_size_in_mb": 0,
-    "rpc_keepalive": true,
-    "rpc_port": 9160,
-    "rpc_server_type": "sync",
-    "saved_caches_directory": "/mnt/cassandra/saved_caches",
-    "snapshot_before_compaction": false,
-    "ssl_storage_port": 7001,
-    "storage_port": 7000,
-    "thrift_framed_transport_size_in_mb": 15,
-    "thrift_max_message_length_in_mb": 16,
-    "trickle_fsync": false,
-    "trickle_fsync_interval_in_kb": 10240
-  },
-  "install_params": {
-    "username": "${ADMIN_USERNAME}",
-    "password": "${ADMIN_PASSWORD}",
-    "package": "dse",
-    "version": "${DSE_VERSION}",
-    "repo-user": "${DATASTAX_USERNAME}",
-    "repo-password": "${DATASTAX_PASSWORD}"
-  },
-  "nodes": [
-    ${NODE_CONFIG_LIST}
-  ],
-  "accepted_fingerprints": {
-    ${ACCEPTED_FINGERPRINTS}
-  }
+   "cassandra_config" : {
+      "auto_bootstrap": false,
+      "commitlog_sync_batch_window_in_ms": 2,
+      "permissions_validity_in_ms" : 2000,
+      "memtable_allocation_type" : "heap_buffers",
+      "column_index_size_in_kb" : 64,
+      "commitlog_sync_period_in_ms" : 10000,
+      "native_transport_max_threads" : 128,
+      "partitioner" : "org.apache.cassandra.dht.Murmur3Partitioner",
+      "ssl_storage_port" : 7001,
+      "authorizer" : "AllowAllAuthorizer",
+      "tombstone_warn_threshold" : 1000,
+      "commitlog_total_space_in_mb" : 8192,
+      "dynamic_snitch_reset_interval_in_ms" : 600000,
+      "tombstone_failure_threshold" : 100000,
+      "cross_node_timeout" : false,
+      "commit_failure_policy" : "stop",
+      "counter_write_request_timeout_in_ms" : 5000,
+      "endpoint_snitch" : "com.datastax.bdp.snitch.DseSimpleSnitch",
+      "request_scheduler" : "org.apache.cassandra.scheduler.NoScheduler",
+      "cas_contention_timeout_in_ms" : 1000,
+      "memtable_heap_space_in_mb" : 2048,
+      "concurrent_reads" : 32,
+      "max_hint_window_in_ms" : 10800000,
+      "start_native_transport" : true,
+      "row_cache_save_period" : 0,
+      "auto_snapshot" : true,
+      "counter_cache_save_period" : 7200,
+      "read_request_timeout_in_ms" : 5000,
+      "saved_caches_directory" : "/var/lib/cassandra/saved_caches",
+      "trickle_fsync_interval_in_kb" : 10240,
+      "data_file_directories" : [
+         "/var/lib/cassandra/data"
+      ],
+      "rpc_port" : 9160,
+      "native_transport_port" : 9042,
+      "start_rpc" : true,
+      "incremental_backups" : false,
+      "dynamic_snitch_update_interval_in_ms" : 100,
+      "concurrent_counter_writes" : 32,
+      "internode_authenticator" : "org.apache.cassandra.auth.AllowAllInternodeAuthenticator",
+      "index_summary_resize_interval_in_minutes" : 60,
+      "row_cache_size_in_mb" : 0,
+      "sstable_preemptive_open_interval_in_mb" : 50,
+      "compaction_throughput_mb_per_sec" : 16,
+      "request_timeout_in_ms" : 10000,
+      "internode_compression" : "dc",
+      "batchlog_replay_throttle_in_kb" : 1024,
+      "disk_failure_policy" : "stop",
+      "rpc_server_type" : "sync",
+      "authenticator" : "AllowAllAuthenticator",
+      "key_cache_save_period" : 14400,
+      "dynamic_snitch_badness_threshold" : 0.1,
+      "trickle_fsync" : false,
+      "commitlog_sync" : "periodic",
+      "concurrent_writes" : 32,
+      "stream_throughput_outbound_megabits_per_sec" : 200,
+      "max_hints_delivery_threads" : 2,
+      "hinted_handoff_enabled" : "true",
+      "memory_allocator" : "NativeAllocator",
+      "rpc_keepalive" : true,
+      "truncate_request_timeout_in_ms" : 60000,
+      "client_encryption_options" : {
+         "keystore" : "resources/dse/conf/.keystore",
+         "protocol" : "TLS",
+         "algorithm" : "SunX509",
+         "keystore_password" : "cassandra",
+         "store_type" : "JKS",
+         "truststore" : "resources/dse/conf/.truststore",
+         "truststore_password" : "cassandra",
+         "enabled" : false,
+         "require_client_auth" : false,
+         "cipher_suites" : [
+            "TLS_RSA_WITH_AES_128_CBC_SHA",
+            "TLS_RSA_WITH_AES_256_CBC_SHA",
+            "TLS_DHE_RSA_WITH_AES_128_CBC_SHA",
+            "TLS_DHE_RSA_WITH_AES_256_CBC_SHA",
+            "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
+            "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA"
+         ]
+      },
+      "hinted_handoff_throttle_in_kb" : 1024,
+      "storage_port" : 7000,
+      "commitlog_segment_size_in_mb" : 32,
+      "native_transport_max_frame_size_in_mb" : 256,
+      "commitlog_directory" : "/var/lib/cassandra/commitlog",
+      "batch_size_warn_threshold_in_kb" : 64,
+      "inter_dc_tcp_nodelay" : false,
+      "snapshot_before_compaction" : false,
+      "thrift_framed_transport_size_in_mb" : 15,
+      "write_request_timeout_in_ms" : 2000,
+      "range_request_timeout_in_ms" : 10000,
+      "memtable_offheap_space_in_mb" : 2048,
+      "cluster_name" : "test01",
+      "server_encryption_options" : {
+         "keystore_password" : "cassandra",
+         "algorithm" : "SunX509",
+         "internode_encryption" : "none",
+         "protocol" : "TLS",
+         "keystore" : "conf/.keystore",
+         "truststore" : "conf/.truststore",
+         "store_type" : "JKS",
+         "truststore_password" : "cassandra",
+         "cipher_suites" : [
+            "TLS_RSA_WITH_AES_128_CBC_SHA",
+            "TLS_RSA_WITH_AES_256_CBC_SHA",
+            "TLS_DHE_RSA_WITH_AES_128_CBC_SHA",
+            "TLS_DHE_RSA_WITH_AES_256_CBC_SHA",
+            "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
+            "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA"
+         ],
+         "require_client_auth" : false
+      }
+   },
+   "is_retry" : false,
+   "install_params" : {
+      "package" : "dse",
+      "private_key" : "",
+      "password" : "${ADMIN_PASSWORD}",
+      "username" : "${ADMIN_USERNAME}",
+      "version" : "${DSE_VERSION}",
+      "repo-password" : "${DATASTAX_PASSWORD}",
+      "repo-user" : "${DATASTAX_USERNAME}"
+   },
+   "local_datacenters" : [
+      {
+         "location" : "",
+         "node_information" : [
+            {
+               "public_ip" : "10.0.100.100",
+               "private_ip" : "10.0.100.100",
+               "node_type" : "cassandra"
+            },
+            {
+               "node_type" : "cassandra",
+               "private_ip" : "10.0.100.101",
+               "public_ip" : "10.0.100.101"
+            }
+         ],
+         "dc" : ""
+      }
+   ]
 }
 EOF
 
