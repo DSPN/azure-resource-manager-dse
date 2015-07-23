308d2a88fba952c6e9355e58a042622d6342977b
benofben <ben.lackey@outlook.com>
2015-07-22 21:42:23 -0700

    trying to roll back to 4.7.0 settings

--- opscenter.sh
+++ opscenter.sh
@@ -141,10 +141,10 @@ sudo tee provision.json > /dev/null <<EOF
 {
   "cassandra_config" : {
     "auto_bootstrap": false,
-    "commitlog_sync_batch_window_in_ms": 2,
     "permissions_validity_in_ms" : 2000,
     "memtable_allocation_type" : "heap_buffers",
     "column_index_size_in_kb" : 64,
+    "commitlog_sync_period_in_ms" : 10000,
     "native_transport_max_threads" : 128,
     "partitioner" : "org.apache.cassandra.dht.Murmur3Partitioner",
     "ssl_storage_port" : 7001,
