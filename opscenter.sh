731155c758774137a6284acdb50d5d30100c3d0a
benofben <ben.lackey@outlook.com>
2015-07-23 10:46:10 -0700

    added -y to apt-get

--- opscenter.sh
+++ opscenter.sh
@@ -60,7 +60,7 @@ echo "Installing Java"
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
 apt-add-repository "deb http://repos.azulsystems.com/ubuntu stable main"
 apt-get update 
-apt-get install zulu-8
+apt-get -y install zulu-8
 
 echo "Installing OpsCenter"
 echo "deb http://debian.datastax.com/community stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.community.list
