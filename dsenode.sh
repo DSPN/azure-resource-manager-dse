f0b2f7ba2251d5b9103f10cfe771f2e988b92ceb
benofben <ben.lackey@outlook.com>
2015-07-23 16:38:57 -0700

    default jre, no zulu

--- dsenode.sh
+++ dsenode.sh
@@ -35,10 +35,7 @@ do
 done
 
 echo "Installing Java"
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
-apt-add-repository "deb http://repos.azulsystems.com/ubuntu stable main"
-apt-get update 
-apt-get -y install zulu-8
+apt-get -y install default-jre
 
 echo "Partitioning and formatting all attached data disks"
 bash vm-disk-utils-0.1.sh
