433a96693685016bbda3b376d1ea6f5fda68a9a8
Ben Lackey <benlackey@Bens-MacBook-Air.local>
2015-07-30 07:13:21 -0700

    removed extra -y

--- install-java.sh
+++ install-java.sh
@@ -9,7 +9,7 @@
 # apt-get -y install oracle-java7-installer
 
 # Install Azul Zulu JDK
-apt-key -y adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
 apt-add-repository -y "deb http://repos.azulsystems.com/ubuntu stable main"
 apt-get -y update 
 apt-get -y install zulu-8
