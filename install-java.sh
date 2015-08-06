07b2397c93955195b75efd1a4996669b6a4d5911
Ben Lackey <ben.lackey@outlook.com>
2015-08-06 16:48:27 -0700

    Update install-java.sh

--- install-java.sh
+++ install-java.sh
@@ -5,7 +5,7 @@
 # add-apt-repository -y ppa:webupd8team/java
 # apt-get -y update 
 # echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-#echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+# echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 # apt-get -y install oracle-java7-installer
 
 # Install Azul Zulu JDK
