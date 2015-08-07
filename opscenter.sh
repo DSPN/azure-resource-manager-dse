a3de8769a05e267f04f7f31f80071fdb47e0c327
benofben <ben.lackey@outlook.com>
2015-08-06 21:24:50 -0700

    major file renaming

--- opscenter.sh
+++ opscenter.sh
@@ -39,8 +39,12 @@ while getopts ":n:u:p:e:v:c:U:P:" opt; do
   esac
 done
 
-echo "Installing Java"
-bash install-java.sh
+
+echo "Installing Azul Zulu JDK"
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
+apt-add-repository -y "deb http://repos.azulsystems.com/ubuntu stable main"
+apt-get -y update
+apt-get -y install zulu-8
 
 echo "Installing OpsCenter"
 echo "deb http://debian.datastax.com/community stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.community.list
