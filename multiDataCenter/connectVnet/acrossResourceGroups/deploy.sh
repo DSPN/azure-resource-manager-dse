91b81a526d07cd4d2bbfe4bca257369c9705e1a1
benofben <ben.lackey@outlook.com>
2015-08-27 16:12:01 -0700

    deleted unusued dirs

--- multiDataCenter/connectVnet/acrossResourceGroups/deploy.sh
+++ multiDataCenter/connectVnet/acrossResourceGroups/deploy.sh
@@ -6,7 +6,7 @@ RG2=$2
 azure group create $1 "East Asia"
 azure group create $2 "West US"
 
-azure group deployment create -f ../../..simple/mainTemplate.json -e ../../..simple/mainTemplateParameters.json $RG1 t0 &
+azure group deployment create -f ../../../simple/mainTemplate.json -e ../../../simple/mainTemplateParameters.json $RG1 t0 &
 azure group deployment create -f ../../simpleAlternateSubnet/mainTemplate.json -e ../../simpleAlternateSubnet/mainTemplateParameters.json $RG2 t0 &
 
 azure group deployment create -f ./vnet1.json -e ./vnet1parameters.json $RG1 t0 &
