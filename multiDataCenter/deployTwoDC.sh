faa8a06005164e7d2358c7d2f3a2744a733a1a60
benofben <ben.lackey@outlook.com>
2015-08-27 14:15:26 -0700

    rename connections

--- multiDataCenter/deployTwoDC.sh
+++ multiDataCenter/deployTwoDC.sh
@@ -3,9 +3,12 @@
 RG1=$1
 RG2=$2
 
-azure group create $1 "East Asia"
-azure group create $2 "West US"
+#azure group create $1 "East Asia"
+#azure group create $2 "West US"
 
-azure group deployment create -f ../simple/mainTemplate.json -e ../simple/mainTemplateParameters.json $RG1 t0 &
-azure group deployment create -f ./simpleAlternateSubnet/mainTemplate.json -e ./simpleAlternateSubnet/mainTemplateParameters.json $RG2 t0 &
+#azure group deployment create -f ../simple/mainTemplate.json -e ../simple/mainTemplateParameters.json $RG1 t0 &
+#azure group deployment create -f ./simpleAlternateSubnet/mainTemplate.json -e ./simpleAlternateSubnet/mainTemplateParameters.json $RG2 t0 &
+
+azure group deployment create -f ./connectVnet/acrossResourceGroups/vnet1.json -e ./connectVnet/acrossResourceGroups/vnet1parameters.json t0 &
+#azure group deployment create -f ./connectVnet/acrossResourceGroups/vnet2.json -e ./connectVnet/acrossResourceGroups/vnet2parameters.json t0 &
 
