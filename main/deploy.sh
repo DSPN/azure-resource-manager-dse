cfe4219d18b4a06a9a261ac1fdf1c2148c66fdaf
benofben <ben.lackey@outlook.com>
2015-09-13 14:58:45 -0700

    defaults don't work.  alternative solution

--- main/deploy.sh
+++ main/deploy.sh
@@ -2,6 +2,8 @@
 
 RESOURCE_GROUP=$1
 azure group create $RESOURCE_GROUP "East Asia"
+azure group deployment create -f ./vnet.json -p "{\"region\": {\"value\": \"East Asia\"}}" $RESOURCE_GROUP t0
+
 #azure group deployment create -f ./mainTemplate.json $RESOURCE_GROUP t0
-azure group deployment create -f ./opsCenterNode.json $RESOURCE_GROUP t0
+#azure group deployment create -f ./opsCenterNode.json $RESOURCE_GROUP t0
 
