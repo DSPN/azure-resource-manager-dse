5678ab0c14cb53355b0bf18e76a58f784fe7cbc5
benofben <ben.lackey@outlook.com>
2015-09-13 16:32:27 -0700

    major rewrite of opscenter.json.  Still need to sort of storage account.

--- main/deploy.sh
+++ main/deploy.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#/bin/sh
 
 RESOURCE_GROUP=$1
-azure group create $RESOURCE_GROUP "East Asia"
-azure group deployment create -f ./vnet.json -p "{\"region\": {\"value\": \"East Asia\"}}" $RESOURCE_GROUP t0
+#azure group create $RESOURCE_GROUP "East Asia"
+#azure group deployment create -f ./vnet.json -p "{\"region\": {\"value\": \"East Asia\"}}" $RESOURCE_GROUP t0
+azure group deployment create -f ./opsCenterNode.json -p "{\"region\": {\"value\": \"East Asia\"}}" $RESOURCE_GROUP t0
+
 
 #azure group deployment create -f ./mainTemplate.json $RESOURCE_GROUP t0
 #azure group deployment create -f ./opsCenterNode.json $RESOURCE_GROUP t0
