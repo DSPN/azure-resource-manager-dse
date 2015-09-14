c8b20bca72e914bea5c869e87070d470d2406795
benofben <ben.lackey@outlook.com>
2015-09-13 21:11:34 -0700

    have opscenter vm setup working now.  will look at nodes tomorrow.

--- main/deploy.sh
+++ main/deploy.sh
@@ -1,11 +1,10 @@
 #/bin/sh
 
 RESOURCE_GROUP=$1
-#azure group create $RESOURCE_GROUP "East Asia"
-#azure group deployment create -f ./vnet.json -p "{\"region\": {\"value\": \"East Asia\"}}" $RESOURCE_GROUP t0
+azure group create $RESOURCE_GROUP "East Asia"
+azure group deployment create -f ./vnet.json -p "{\"region\": {\"value\": \"East Asia\"}}" $RESOURCE_GROUP dse
+azure group deployment create -f ./opsCenter.json -p "{\"username\": {\"value\": \"datastax\"}, \"password\": {\"value\": \"foo123!\"}, \"region\": {\"value\": \"East Asia\"}}" $RESOURCE_GROUP dse
 
-azure group deployment create -f ./opsCenter.json -p "{\"username\": {\"value\": \"datastax\"}, \"password\": {\"value\": \"foo123!\"}, \"region\": {\"value\": \"East Asia\"}}" $RESOURCE_GROUP t0
-
-#azure group deployment create -f ./mainTemplate.json $RESOURCE_GROUP t0
-#azure group deployment create -f ./opsCenterNode.json $RESOURCE_GROUP t0
+#azure group deployment create -f ./mainTemplate.json $RESOURCE_GROUP dse
+#azure group deployment create -f ./opsCenterNode.json $RESOURCE_GROUP dse
 
