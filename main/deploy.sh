9375441d542f9678d5f75a9866b76efe8f752955
benofben <ben.lackey@outlook.com>
2015-09-19 14:21:48 -0700

    started on skeleton

--- main/deploy.sh
+++ main/deploy.sh
@@ -2,8 +2,23 @@
 
 RESOURCE_GROUP=$1
 azure group create $RESOURCE_GROUP "East Asia"
-azure group deployment create -f ./vnet.json -p "{\"region\": {\"value\": \"East Asia\"}}" $RESOURCE_GROUP dse
-azure group deployment create -f ./opsCenter.json -p "{\"username\": {\"value\": \"datastax\"}, \"password\": {\"value\": \"foo123!\"}, \"region\": {\"value\": \"East Asia\"}}" $RESOURCE_GROUP dse
+
+# This writes our generated template to template.json
+python buildTemplate.py input.json
+
+#azure group deployment create -f ./template.json $RESOURCE_GROUP dse
+
+
+
+
+
+
+#########
+### old
+#########
+
+#azure group deployment create -f ./vnet.json -p "{\"region\": {\"value\": \"East Asia\"}}" $RESOURCE_GROUP dse
+#azure group deployment create -f ./opsCenter.json -p "{\"username\": {\"value\": \"datastax\"}, \"password\": {\"value\": \"foo123!\"}, \"region\": {\"value\": \"East Asia\"}}" $RESOURCE_GROUP dse
 
 #azure group deployment create -f ./mainTemplate.json $RESOURCE_GROUP dse
 #azure group deployment create -f ./opsCenterNode.json $RESOURCE_GROUP dse
