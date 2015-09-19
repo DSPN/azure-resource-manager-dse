9e3a9c357107f5993ed09f2b50e66cd26ac1e9d5
benofben <ben.lackey@outlook.com>
2015-09-19 14:24:22 -0700

    renamed some things

--- main/deploy.sh
+++ main/deploy.sh
@@ -3,10 +3,10 @@
 RESOURCE_GROUP=$1
 azure group create $RESOURCE_GROUP "East Asia"
 
-# This writes our generated template to template.json
+# This writes our generated template to mainTemplate.json
 python buildTemplate.py input.json
 
-#azure group deployment create -f ./template.json $RESOURCE_GROUP dse
+#azure group deployment create -f ./mainTemplate.json $RESOURCE_GROUP dse
 
 
 
