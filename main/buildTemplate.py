0a2e6e6fcbf0011b79ce1ff6f14b0977b81ebeb1
benofben <ben.lackey@outlook.com>
2015-09-19 18:30:25 -0700

    commets in py file

--- main/buildTemplate.py
+++ main/buildTemplate.py
@@ -1,3 +1,21 @@
 import sys
 print  sys.argv[1:]
 
+# This python script generates an ARM template that deploys DSE across regions.  Arguments are passed in as a json file.
+#
+# First off we create the infrastructure that the OpsCenter node requires:
+# (1) Vnet and a subnet within that to deploy to.
+# (2) Public IP Address for OpsCenter
+# (3) VM
+# (4) Storage Account
+#
+# Then we loop through for each data center and create:
+# (1) Vnet and a subnet within that to deploy to.
+# (2) nodesPerRegion number of VMs
+# (3) math.ceil(nodesPerRegion/40.0) number of storage accounts
+
+# Then we loop across the Vnets (OpsCenter and the DSE nodes) and connect those all togther.  This requires creating:
+# (1) Gateways in each Vnet
+# (2) ...
+
+
