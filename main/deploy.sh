6ad569749eebafa57c96d0c8f37731b53fdf6782
benofben <ben.lackey@outlook.com>
2015-09-19 21:12:49 -0700

    deleted some unused commands that have been replace by py

--- main/deploy.sh
+++ main/deploy.sh
@@ -3,20 +3,8 @@
 RESOURCE_GROUP=$1
 azure group create $RESOURCE_GROUP "East Asia"
 
-# This writes output to generatedTemplate.json
+# This writes output to generatedTemplate.json using clusterParameters.json as input
 python main.py
 
 azure group deployment create -f ./generatedTemplate.json $RESOURCE_GROUP dse
-
-
-
-#########
-### old
-#########
-
-#azure group deployment create -f ./vnet.json -p "{\"region\": {\"value\": \"East Asia\"}}" $RESOURCE_GROUP dse
-#azure group deployment create -f ./opsCenter.json -p "{\"username\": {\"value\": \"datastax\"}, \"password\": {\"value\": \"foo123!\"}, \"region\": {\"value\": \"East Asia\"}}" $RESOURCE_GROUP dse
-
-#azure group deployment create -f ./mainTemplate.json $RESOURCE_GROUP dse
-#azure group deployment create -f ./opsCenterNode.json $RESOURCE_GROUP dse
 
