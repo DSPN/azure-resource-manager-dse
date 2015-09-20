081981140736ad076c1faf09cd5e2c4e89cb226a
benofben <ben.lackey@outlook.com>
2015-09-19 23:45:14 -0700

    added connections template

--- main/connections.py
+++ main/connections.py
@@ -1,5 +1,152 @@
 def generate_template(region, nodeSize, nodesPerRegion, username, password):
-    # (1) Gateways in each Vnet
-    # (2) ...
-
     return []
+
+
+resources = [
+    {
+        "name": "[parameters('virtualNetworkName1')]",
+        "type": "Microsoft.Network/virtualNetworks",
+        "location": "[parameters('location1')]",
+        "apiVersion": "2015-05-01-preview",
+        "properties": {
+            "addressSpace": {
+                "addressPrefixes": [
+                    "[parameters('virtualNetworkAddressRange1')]"
+                ]
+            },
+            "subnets": "[parameters('subnets1')]"
+        }
+    },
+    {
+        "name": "[parameters('virtualNetworkName2')]",
+        "type": "Microsoft.Network/virtualNetworks",
+        "location": "[parameters('location2')]",
+        "apiVersion": "2015-05-01-preview",
+        "properties": {
+            "addressSpace": {
+                "addressPrefixes": [
+                    "[parameters('virtualNetworkAddressRange2')]"
+                ]
+            },
+            "subnets": "[parameters('subnets2')]"
+        }
+    },
+    {
+        "apiVersion": "2015-05-01-preview",
+        "type": "Microsoft.Network/publicIPAddresses",
+        "name": "GWIPAddress1",
+        "location": "[parameters('location1')]",
+        "properties": {
+            "publicIPAllocationMethod": "Dynamic"
+        }
+    },
+    {
+        "apiVersion": "2015-05-01-preview",
+        "type": "Microsoft.Network/publicIPAddresses",
+        "name": "GWIPAddress2",
+        "location": "[parameters('location2')]",
+        "properties": {
+            "publicIPAllocationMethod": "Dynamic"
+        }
+    },
+    {
+        "apiVersion": "2015-05-01-preview",
+        "type": "Microsoft.Network/virtualNetworkGateways",
+        "name": "[parameters('gatewayName1')]",
+        "location": "[parameters('location1')]",
+        "dependsOn": [
+            "Microsoft.Network/publicIPAddresses/GWIPAddress1",
+            "[concat('Microsoft.Network/virtualNetworks/', parameters('virtualNetworkName1'))]"
+        ],
+        "properties": {
+            "ipConfigurations": [
+                {
+                    "properties": {
+                        "privateIPAllocationMethod": "Dynamic",
+                        "subnet": {
+                            "id": "[variables('gwSubnetRef1')]"
+                        },
+                        "publicIPAddress": {
+                            "id": "[resourceId('Microsoft.Network/publicIPAddresses','GWIPAddress1')]"
+                        }
+                    },
+                    "name": "vnetGatewayConfig"
+                }
+            ],
+            "gatewayType": "Vpn",
+            "vpnType": "RouteBased",
+            "enableBgp": false
+        }
+    },
+    {
+        "apiVersion": "2015-05-01-preview",
+        "type": "Microsoft.Network/virtualNetworkGateways",
+        "name": "[parameters('gatewayName2')]",
+        "location": "[parameters('location2')]",
+        "dependsOn": [
+            "Microsoft.Network/publicIPAddresses/GWIPAddress2",
+            "[concat('Microsoft.Network/virtualNetworks/', parameters('virtualNetworkName2'))]"
+        ],
+        "properties": {
+            "ipConfigurations": [
+                {
+                    "properties": {
+                        "privateIPAllocationMethod": "Dynamic",
+                        "subnet": {
+                            "id": "[variables('gwSubnetRef2')]"
+                        },
+                        "publicIPAddress": {
+                            "id": "[resourceId('Microsoft.Network/publicIPAddresses','GWIPAddress2')]"
+                        }
+                    },
+                    "name": "vnetGatewayConfig"
+                }
+            ],
+            "gatewayType": "Vpn",
+            "vpnType": "RouteBased",
+            "enableBgp": false
+        }
+    },
+    {
+        "apiVersion": "2015-05-01-preview",
+        "type": "Microsoft.Network/connections",
+        "name": "connection1",
+        "location": "[parameters('location1')]",
+        "dependsOn": [
+            "[concat('Microsoft.Network/virtualNetworkGateways/', parameters('gatewayName1'))]",
+            "[concat('Microsoft.Network/virtualNetworkGateways/', parameters('gatewayName2'))]"
+        ],
+        "properties": {
+            "virtualNetworkGateway1": {
+                "id": "[variables('vnetGatewayID1')]"
+            },
+            "virtualNetworkGateway2": {
+                "id": "[variables('vnetGatewayID2')]"
+            },
+            "connectionType": "Vnet2Vnet",
+            "routingWeight": 3,
+            "sharedKey": "[parameters('gatewaySharedKey')]"
+        }
+    },
+    {
+        "apiVersion": "2015-05-01-preview",
+        "type": "Microsoft.Network/connections",
+        "name": "connection2",
+        "location": "[parameters('location2')]",
+        "dependsOn": [
+            "[concat('Microsoft.Network/virtualNetworkGateways/', parameters('gatewayName1'))]",
+            "[concat('Microsoft.Network/virtualNetworkGateways/', parameters('gatewayName2'))]"
+        ],
+        "properties": {
+            "virtualNetworkGateway1": {
+                "id": "[variables('vnetGatewayID2')]"
+            },
+            "virtualNetworkGateway2": {
+                "id": "[variables('vnetGatewayID1')]"
+            },
+            "connectionType": "Vnet2Vnet",
+            "routingWeight": 3,
+            "sharedKey": "[parameters('gatewaySharedKey')]"
+        }
+    }
+]
--- main/dseNodes.py
+++ main/dseNodes.py
@@ -1,6 +1,6 @@
 def generate_template(region, nodeSize, nodesPerRegion, username, password):
     # (1) Vnet and a subnet within that to deploy to.
-    # (2) nodesPerRegion number of VMs
-    # (3) math.ceil(nodesPerRegion/40.0) number of storage accounts
+    # (2) math.ceil(nodesPerRegion/40.0) number of storage accounts
+    # (3) nodesPerRegion number of VMs
 
     return []
