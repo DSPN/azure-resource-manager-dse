e4555c31f9c4c6cd89fd317d2c58d7ab2c640d6d
benofben <ben.lackey@outlook.com>
2015-09-21 10:49:06 -0700

    specified opscenter version to avoid breaking when api changes

--- main/scripts/opsCenter.sh
+++ main/scripts/opsCenter.sh
@@ -11,7 +11,7 @@ echo "Installing OpsCenter"
 echo "deb http://debian.datastax.com/community stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.community.list
 curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -
 apt-get update
-apt-get -y install opscenter
+apt-get -y install opscenter=5.2.1
 
 echo "Starting OpsCenter"
 sudo service opscenterd start
