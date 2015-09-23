7e81708cc84804343f6f21642d1f697f2a6c024f
benofben <ben.lackey@outlook.com>
2015-09-22 23:10:19 -0700

    OpsC to 5.2.1 rather than latest

--- marketplace/opscenter.sh
+++ marketplace/opscenter.sh
@@ -44,7 +44,7 @@ echo "Installing OpsCenter"
 echo "deb http://debian.datastax.com/community stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.community.list
 curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -
 apt-get update
-apt-get -y install opscenter
+apt-get -y install opscenter=5.2.1
 
 # Enable authentication in /etc/opscenter/opscenterd.conf
 sed -i '/^\[authentication\]$/,/^\[/ s/^enabled = False/enabled = True/' /etc/opscenter/opscenterd.conf
