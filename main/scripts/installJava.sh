f0df2f70716b040122df0d656e9a3ce8d74d1e90
benofben <ben.lackey@outlook.com>
2015-10-06 12:58:46 -0700

    using openjdk 7 instead of Azul

--- main/scripts/installJava.sh
+++ main/scripts/installJava.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-echo "Installing Azul Zulu JDK"
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
-apt-add-repository -y "deb http://repos.azulsystems.com/ubuntu stable main"
+echo "Installing OpenJDK 7"
 apt-get -y update
-apt-get -y install zulu-8
+apt-get -y install openjdk-7-jdk
+
+#echo "Installing Azul Zulu JDK"
+#apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
+#apt-add-repository -y "deb http://repos.azulsystems.com/ubuntu stable main"
+#apt-get -y update
+#apt-get -y install zulu-8
 
