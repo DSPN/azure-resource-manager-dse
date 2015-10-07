eb2b4d92d05531c334890721ba20372bf5a2b032
benofben <ben.lackey@outlook.com>
2015-10-07 12:15:07 -0700

    back to Azul since Canonical repos are timing out.

--- simple/opscenter.sh
+++ simple/opscenter.sh
@@ -34,15 +34,11 @@ while getopts ":n:u:p:e:c:U:P:" opt; do
   esac
 done
 
-echo "Installing OpenJDK 7"
+echo "Installing Azul Zulu JDK"
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
+apt-add-repository -y "deb http://repos.azulsystems.com/ubuntu stable main"
 apt-get -y update
-apt-get -y install openjdk-7-jdk
-
-#echo "Installing Azul Zulu JDK"
-#apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
-#apt-add-repository -y "deb http://repos.azulsystems.com/ubuntu stable main"
-#apt-get -y update
-#apt-get -y install zulu-8
+apt-get -y install zulu-8
 
 echo "Installing OpsCenter"
 echo "deb http://debian.datastax.com/community stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.community.list
