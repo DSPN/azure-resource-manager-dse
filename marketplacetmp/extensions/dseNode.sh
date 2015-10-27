bb5af3cc42a42b56d69a85dd063a388e4730cb72
benofben <ben.lackey@outlook.com>
2015-10-26 22:20:42 -0600

    remove install java script

--- marketplacetmp/extensions/dseNode.sh
+++ marketplacetmp/extensions/dseNode.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-bash installJava.sh
+echo "Installing Azul Zulu JDK"
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
+apt-add-repository -y "deb http://repos.azulsystems.com/ubuntu stable main"
+apt-get -y update
+apt-get -y install zulu-8
 
 echo "Partitioning and formatting all attached data disks"
 bash vm-disk-utils-0.1.sh
