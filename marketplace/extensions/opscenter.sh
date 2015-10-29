62b97537af725d6251e82e72044791bcb0a1d0d1
benofben <ben.lackey@outlook.com>
2015-10-29 09:02:02 -0600

    OpsCenter versions to 5.2.2

--- marketplace/extensions/opscenter.sh
+++ marketplace/extensions/opscenter.sh
@@ -36,7 +36,7 @@ echo "Installing OpsCenter"
 echo "deb http://debian.datastax.com/community stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.community.list
 curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -
 apt-get update
-apt-get -y install opscenter=5.2.1
+apt-get -y install opscenter=5.2.2
 
 echo "Starting OpsCenter"
 sudo service opscenterd start
