d5613b51dbc3e7331c79fc33e5d67e8dd21c0240
benofben <ben.lackey@outlook.com>
2015-10-29 21:40:24 -0600

    Switch to Oracle JDK

--- buildImage.sh
+++ buildImage.sh
@@ -22,12 +22,13 @@ mount /dev/mapper/loop0p1 /mnt2
 mv /mnt2/etc/resolv.conf /mnt2/etc/resolv.conf.bak
 cp /etc/resolv.conf /mnt2/etc/resolv.conf
 chroot /mnt2
- 
-# Install Azul Zulu JDK
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
-apt-add-repository -y "deb http://repos.azulsystems.com/ubuntu stable main"
-apt-get -y update
-apt-get -y install zulu-8
+
+#Install Java
+add-apt-repository -y ppa:webupd8team/java
+apt-get -y update 
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+apt-get -y install oracle-java8-installer
  
 # Exit the chroot and unmount
 rm /etc/resolv.conf
