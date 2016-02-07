63a45f226df202d413cb6e7c5f2a35038ca6c2bf
benofben <ben.lackey@outlook.com>
2016-02-07 12:09:47 -0800

    drop vpn gateways
    we’re going to use public IPs instead

--- multidc/main.py
+++ multidc/main.py
@@ -1,9 +1,8 @@
 import json
 import opsCenterNode
 import dseNodes
-import connections
 
-# This python script generates an ARM template that deploys DSE across locations.
+# This python script generates an ARM template that deploys DSE across multiple datacenters.
 
 with open('clusterParameters.json') as inputFile:
     clusterParameters = json.load(inputFile)
@@ -13,6 +12,7 @@ nodeSize = clusterParameters['nodeSize']
 nodesPerLocation = clusterParameters['nodesPerLocation']
 username = clusterParameters['username']
 password = clusterParameters['password']
+nodeType = clusterParameters['nodeType']
 
 # This is the skeleton of the template that we're going to add resources to
 generatedTemplate = {
@@ -33,13 +33,11 @@ for location in locations:
     resources = dseNodes.generate_template(location, datacenterIndex, nodeSize, nodesPerLocation, username, password)
     generatedTemplate['resources'] += resources
 
-# Connect the locations together
-resources = connections.generate_template(locations)
-generatedTemplate['resources'] += resources
-
 # Create the OpsCenter node
 resources = opsCenterNode.generate_template(clusterParameters)
 generatedTemplate['resources'] += resources
 
 with open('generatedTemplate.json', 'w') as outputFile:
     json.dump(generatedTemplate, outputFile, sort_keys=True, indent=4, ensure_ascii=False)
+
+# Populate the opsCenterURL in outputs
