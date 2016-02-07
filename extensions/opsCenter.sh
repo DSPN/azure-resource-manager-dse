19b25f0612c724462f2411dfdd19c41de4dceede
benofben <ben.lackey@outlook.com>
2016-02-06 22:46:33 -0800

    just hacking

--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# This script is used by both single and multi dc deploys.
-# The multidc passes a b64 json blob.
-# The singledc passes 5 command line arguments.
-
-# save our b64 encoded json parameters for later
-echo $1 > b64input
-
 bash installJava.sh
 
 echo "Installing OpsCenter"
@@ -20,9 +13,6 @@ sudo service opscenterd start
 
 echo "Waiting for OpsCenter to start..."
 sleep 15
-
-echo "Python script needs sshpass to determine local IPs."
-apt-get -y install sshpass
 
 echo "Generating a provision.json file"
 python opsCenter.py $1 $2 $3 $4 $5
