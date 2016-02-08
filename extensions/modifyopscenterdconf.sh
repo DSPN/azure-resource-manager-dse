8c575ee9d7225d9983419aae16044192df6e4189
benofben <ben.lackey@outlook.com>
2016-02-08 13:48:26 -0800

    ip print bug and fiddle with agent conf files

--- extensions/modifyopscenterdconf.sh
+++ extensions/modifyopscenterdconf.sh
@@ -4,7 +4,7 @@
 # The agents on nodes won't be able to connect to that.
 
 PUBLICIP=`curl --max-time 50000 --retry 12 --retry-delay 50000 -s 'http://checkip.dyndns.org' | sed 's/.*Current IP Address: \([0-9\.]*\).*/\1/g'`
-echo "My public IP address is $PUBLICIP"
+echo 'My public IP address is '$PUBLICIP
 
 echo "Making changes to the OpsCenter config based on my public IP."
 echo '[agents]' >> /etc/opscenter/opscenterd.conf
--- extensions/node.sh
+++ extensions/node.sh
@@ -5,3 +5,11 @@ bash installJava.sh
 
 echo "Modifying permissions"
 chmod 777 /mnt
+
+# This is a bit of a hack that should go away with OpsCenter 6.x provisioning
+
+PUBLICIP=`curl --max-time 50000 --retry 12 --retry-delay 50000 -s 'http://checkip.dyndns.org' | sed 's/.*Current IP Address: \([0-9\.]*\).*/\1/g'`
+echo 'My public IP address is '$PUBLICIP
+
+echo "We need to tell the datastax-agent to bind to its publicip."
+echo 'agent_rpc_broadcast_address: '$PUBLICIP >> /var/lib/datastax-agent/conf/address.yaml
