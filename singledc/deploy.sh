276786d4b804de422f26fb588d54a1b8fc2f6a13
benofben <ben.lackey@outlook.com>
2016-02-07 20:01:29 -0800

    machine friendly location

--- singledc/deploy.sh
+++ singledc/deploy.sh
@@ -2,6 +2,6 @@
 
 RESOURCE_GROUP=$1
 
-azure group create $1 "East Asia"
+azure group create $1 "eastasia"
 azure group deployment create -f mainTemplate.json -e mainTemplateParameters.json $RESOURCE_GROUP dse
 
