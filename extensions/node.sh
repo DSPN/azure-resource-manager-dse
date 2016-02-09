4deada5f88941eb8ab5eb969554db05692a464cb
benofben <ben.lackey@outlook.com>
2016-02-09 09:48:13 -0800

    drop ext scripts

--- extensions/node.sh
+++ extensions/node.sh
@@ -5,6 +5,3 @@ bash installJava.sh
 
 echo "Modifying permissions"
 chmod 777 /mnt
-
-# This is a bit of a hack that should go away with OpsCenter 6.x provisioning
-bash set_agent_rpc_broadcast_address.sh
