c6fc042c07fe9244c02641d061480f8637641d45
benofben <ben.lackey@outlook.com>
2016-02-11 22:44:04 -0800

    Use Oracle JDK

--- extensions/installJava.sh
+++ extensions/installJava.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo "Installing Azul Zulu JDK"
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
-apt-add-repository -y "deb http://repos.azulsystems.com/ubuntu stable main"
+echo "Installing the Oracle JDK"
+add-apt-repository -y ppa:webupd8team/java
 apt-get -y update
-apt-get -y install zulu-8
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+apt-get -y install oracle-java7-installer
