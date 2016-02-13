6eaba80347c2de11f9c56289f8810791c27afd1a
benofben <ben.lackey@outlook.com>
2016-02-13 14:00:38 -0800

    installing unzip

--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 wget https://github.com/DSPN/install-datastax/archive/master.zip
+apt-get -y install unzip
 unzip install-datastax-master.zip
 cd install-datastax-master/bin
+
 ./opscenter.sh
