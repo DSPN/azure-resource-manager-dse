1fcd33b2182c1bcfe1135d80259705f3d55480b5
benofben <ben.lackey@outlook.com>
2016-02-14 23:30:24 -0800

    pass parameters to scripts

--- extensions/node.sh
+++ extensions/node.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
+cloud_type="azure"
+data_center_name=$1
+location=$2
+uniqueString=$3
+
+seed_node_dns_name="dc0vm0$uniqueString.$location.cloudapp.azure.com"
+seed_node_public_ip=$seed_node_dns_name
+
+opscenter_dns_name="opscenter$uniqueString.$location.cloudapp.azure.com"
+opscenter_public_ip=$opscenter_dns_name
+
 wget https://github.com/DSPN/install-datastax/archive/master.zip
 apt-get -y install unzip
 unzip master.zip
 cd install-datastax-master/bin
 
-./dse.sh
+./dse.sh $cloud_type $data_center_name $seed_node_public_ip $opscenter_public_ip
--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+location=$1
+uniqueString=$2
+
+seed_node_dns_name="dc0vm0$uniqueString.$location.cloudapp.azure.com"
+seed_node_public_ip=$seed_node_dns_name
+
 wget https://github.com/DSPN/install-datastax/archive/master.zip
 apt-get -y install unzip
 unzip master.zip
 cd install-datastax-master/bin
 
-./opscenter.sh
+./opscenter.sh $seed_node_public_ip
