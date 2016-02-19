17463346181f3bd50f33b8afef1dda38a1e250fb
benofben <ben.lackey@outlook.com>
2016-02-19 12:48:33 -0800

    drops opsc from agent

--- extensions/node.sh
+++ extensions/node.sh
@@ -2,18 +2,15 @@
 
 cloud_type="azure"
 data_center_name=$1
-location=$2 #this is the location of the seed and OpsCenter, not of this node
+location=$2 #this is the location of the seed and OpsCenter, not necessarily of this node
 uniqueString=$3
 
 seed_node_dns_name="dc0vm0$uniqueString.$location.cloudapp.azure.com"
 seed_node_public_ip=`dig +short $seed_node_dns_name | awk '{ print ; exit }'`
-
-opscenter_dns_name="opscenter$uniqueString.$location.cloudapp.azure.com"
-opscenter_public_ip=`dig +short $opscenter_dns_name | awk '{ print ; exit }'`
 
 wget https://github.com/DSPN/install-datastax/archive/master.zip
 apt-get -y install unzip
 unzip master.zip
 cd install-datastax-master/bin
 
-./dse.sh $cloud_type $data_center_name $seed_node_public_ip $opscenter_public_ip
+./dse.sh $cloud_type $data_center_name $seed_node_public_ip
