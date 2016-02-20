8dfed4fa69186ba0070c1ad19fc1709a08823a68
benofben <ben.lackey@outlook.com>
2016-02-19 20:02:04 -0800

    more zip work

--- marketplace/makeArchive.sh
+++ marketplace/makeArchive.sh
@@ -9,11 +9,15 @@ cp ../../extensions/* ./
 cp ../../singledc/* ./
 
 # Do this after since we're going to overwrite mainTemplate.json
-cp ../../mainTemplate.json ./
-cp ../../createUiDefinition.json ./
+cp ../mainTemplate.json ./
+cp ../createUiDefinition.json ./
 
 # Drop some files that don't need to be in the archive
 rm README.md
 rm deploy.sh
 rm mainTemplateParameters.json
+
+zip ../archive.zip *
+cd -
+rm -rf tmp
 
