f1f74a8b8a252a4ad1d80fc50c1bbf9247c53596
benofben <ben.lackey@outlook.com>
2016-02-28 14:19:01 -0800

    move dos resolution to install-datastax repo

--- extensions/node.sh
+++ extensions/node.sh
@@ -6,11 +6,10 @@ location=$2 #this is the location of the seed and OpsCenter, not necessarily of 
 uniqueString=$3
 
 seed_node_dns_name="dc0vm0$uniqueString.$location.cloudapp.azure.com"
-seed_node_public_ip=`dig +short $seed_node_dns_name | awk '{ print ; exit }'`
 
 wget https://github.com/DSPN/install-datastax/archive/master.zip
 apt-get -y install unzip
 unzip master.zip
 cd install-datastax-master/bin
 
-./dse.sh $cloud_type $data_center_name $seed_node_public_ip
+./dse.sh $cloud_type $data_center_name $seed_node_dns_name
--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -4,11 +4,10 @@ location=$1
 uniqueString=$2
 
 seed_node_dns_name="dc0vm0$uniqueString.$location.cloudapp.azure.com"
-seed_node_public_ip=`dig +short $seed_node_dns_name | awk '{ print ; exit }'`
 
 wget https://github.com/DSPN/install-datastax/archive/master.zip
 apt-get -y install unzip
 unzip master.zip
 cd install-datastax-master/bin
 
-./opscenter.sh $seed_node_public_ip
+./opscenter.sh $seed_node_dns_name
