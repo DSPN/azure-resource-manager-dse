c20ecaa17345a537664f8c1f1706a44da6f9abec
benofben <ben.lackey@outlook.com>
2016-08-22 22:42:03 -0700

    revert to pre-vm

--- marketplace/makeArchive.sh
+++ marketplace/makeArchive.sh
@@ -6,11 +6,16 @@ mkdir tmp
 cd tmp
 
 cp ../../extensions/* ./
+cp ../../singledc/* ./
 
+# Do this after since we're going to overwrite mainTemplate.json
 cp ../mainTemplate.json ./
-cp ../nodes.json ./
-cp ../opsCenter.json ./
 cp ../createUiDefinition.json ./
+
+# Drop some files that don't need to be in the archive
+rm README.md
+rm deploy.sh
+rm mainTemplateParameters.json
 
 zip ../archive.zip *
 cd -
