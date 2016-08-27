7569e6e5520f1741fa3f3c4225f440e2e6f3b14b
benofben <ben.lackey@outlook.com>
2016-08-27 16:33:10 -0500

    pass dse version from sh

--- extensions/node.sh
+++ extensions/node.sh
@@ -5,12 +5,14 @@ seed_node_location=$1
 unique_string=$2
 data_center_name=$3
 opscenter_location=$4
+dse_version=4.8.9-1
 
 echo "Input to node.sh is:"
 echo cloud_type $cloud_type
 echo seed_node_location $seed_node_location
 echo unique_string $unique_string
 echo opscenter_location $opscenter_location
+echo dse_version $dse_version
 
 seed_node_dns_name="dc0vm0$unique_string.$seed_node_location.cloudapp.azure.com"
 opscenter_dns_name="opscenter$unique_string.$opscenter_location.cloudapp.azure.com"
@@ -20,11 +22,16 @@ echo cloud_type $cloud_type
 echo seed_node_dns_name $seed_node_dns_name
 echo data_center_name $data_center_name
 echo opscenter_dns_name $opscenter_dns_name
+echo dse_version $dse_version
 
 apt-get -y install unzip
 
-wget https://github.com/DSPN/install-datastax-ubuntu/archive/5.0.1-4.zip
-unzip 5.0.1-4.zip
-cd install-datastax-ubuntu-5.0.1-4/bin
+#wget https://github.com/DSPN/install-datastax-ubuntu/archive/5.0.1-4.zip
+#unzip 5.0.1-4.zip
+#cd install-datastax-ubuntu-5.0.1-4/bin
 
-./dse.sh $cloud_type $seed_node_dns_name $data_center_name $opscenter_dns_name
+wget https://github.com/DSPN/install-datastax-ubuntu/archive/master.zip
+unzip master.zip
+cd install-datastax-ubuntu/bin
+
+./dse.sh $cloud_type $seed_node_dns_name $data_center_name $opscenter_dns_name $dse_version
