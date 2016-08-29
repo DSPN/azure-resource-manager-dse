15d6368625afa968135e2ff96677cad4262ba988
benofben <ben.lackey@outlook.com>
2016-08-29 11:41:14 -0700

    point at master

--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -17,8 +17,12 @@ echo seed_node_dns_name $seed_node_dns_name
 
 apt-get -y install unzip
 
-wget https://github.com/DSPN/install-datastax-ubuntu/archive/5.0.1-4.zip
-unzip 5.0.1-4.zip
-cd install-datastax-ubuntu-5.0.1-4/bin
+#wget https://github.com/DSPN/install-datastax-ubuntu/archive/5.0.1-4.zip
+#unzip 5.0.1-4.zip
+#cd install-datastax-ubuntu-5.0.1-4/bin
+
+wget https://github.com/DSPN/install-datastax-ubuntu/archive/master.zip
+unzip master.zip
+cd install-datastax-ubuntu-master/bin
 
 ./opscenter.sh $cloud_type $seed_node_dns_name
