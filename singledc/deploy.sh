4d22ede2428bc9a289a1c0d88ea37a0a09ffc05f
Collin Poczatek <collin.poczatek@gmail.com>
2017-03-14 21:22:19 -0700

    merge in parts from lcm branch

--- singledc/deploy.sh
+++ singledc/deploy.sh
@@ -2,5 +2,5 @@
 
 RESOURCE_GROUP=$1
 
-azure group create $1 "eastasia"
+azure group create $1 "eastus"
 azure group deployment create -f mainTemplate.json -e mainTemplateParameters.json $RESOURCE_GROUP dse
