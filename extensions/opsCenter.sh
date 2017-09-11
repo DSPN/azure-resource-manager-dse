f0d0e36c5f770dd93c6797aaa6ae41411eda8ede
Collin Poczatek <collin.poczatek@gmail.com>
2017-09-11 16:25:24 -0700

    set DSE ver 5.1.3

--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -37,7 +37,7 @@ cd install-datastax-ubuntu-$release/bin
 
 # Overide OpsC install default version if needed
 export OPSC_VERSION='6.1.2'
-ver='5.1.2'
+ver='5.1.3'
 
 ./os/install_java.sh
 ./opscenter/install.sh
