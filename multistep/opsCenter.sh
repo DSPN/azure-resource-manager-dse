4fe417c0cc786484a78b2330036d28d2598343c3
Collin Poczatek <collin.poczatek@gmail.com>
2018-01-18 11:27:24 -0800

    test rhel shell changes

--- multistep/opsCenter.sh
+++ multistep/opsCenter.sh
@@ -9,28 +9,45 @@ echo username $username
 echo password XXXXXX
 echo cluster_name $cluster_name
 
+##### Install required OS packages
+yum -y update
+yum -y install unzip wget
+#wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
+wget http://mirror.centos.org/centos/7/extras/x86_64/Packages/epel-release-7-9.noarch.rpm
+rpm -ivh epel-release-7-9.noarch.rpm
+yum -y install python-pip
+pip install requests
+
 # repo creds
 repouser='datastax@microsoft.com'
 repopw='3A7vadPHbNT'
 
-release="6.0.3"
-wget https://github.com/DSPN/install-datastax-ubuntu/archive/$release.tar.gz
+release="0.1.9"
+wget https://github.com/DSPN/install-datastax-redhat/archive/$release.tar.gz
 tar -xvf $release.tar.gz
 
-cd install-datastax-ubuntu-$release/bin
-# install extra packages
-./os/extra_packages.sh
-
-# Overide OpsC install default version if needed
-export OPSC_VERSION='6.1.5'
-ver='5.1.5'
+cd install-datastax-redhat-$release/bin
 
 ./os/install_java.sh
 
 #install opsc
 ./opscenter/install.sh 'azure'
 ./opscenter/start.sh
+cd ../..
+echo "Sleeping 1m..."
 sleep 1m
+
+# Overide OpsC install default version if needed
+export OPSC_VERSION='6.1.5'
+ver='5.1.5'
+
+release="6.0.3"
+wget https://github.com/DSPN/install-datastax-ubuntu/archive/$release.tar.gz
+tar -xvf $release.tar.gz
+
+cd install-datastax-ubuntu-$release/bin
+
+exit 0
 
 echo "Calling setupCluster.py with the settings:"
 echo opsc_ip 127.0.0.1
