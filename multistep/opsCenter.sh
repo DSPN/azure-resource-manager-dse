f3e0d9e231eb0fdbe552fa8260f67cafe998d1a1
Collin Poczatek <collin.poczatek@gmail.com>
2018-02-02 11:41:15 -0800

    version updates

--- multistep/opsCenter.sh
+++ multistep/opsCenter.sh
@@ -13,7 +13,7 @@ echo cluster_name $cluster_name
 repouser='datastax@microsoft.com'
 repopw='3A7vadPHbNT'
 
-release="6.0.3"
+release="6.0.4"
 wget https://github.com/DSPN/install-datastax-ubuntu/archive/$release.tar.gz
 tar -xvf $release.tar.gz
 
@@ -23,7 +23,7 @@ cd install-datastax-ubuntu-$release/bin
 
 # Overide OpsC install default version if needed
 export OPSC_VERSION='6.1.5'
-ver='5.1.5'
+ver='5.1.6'
 
 ./os/install_java.sh
 
