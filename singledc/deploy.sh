185e4b8cab352aba5d3c42a4b8490e1b6221ccb8
Collin Poczatek <collin.poczatek@gmail.com>
2018-04-02 10:19:04 -0700

    hardcode workshop branch

--- singledc/deploy.sh
+++ singledc/deploy.sh
@@ -42,21 +42,10 @@ fi
 echo "CLI v2 'az' command found"
 echo "Using values: resource_group=$resource_group location=$location"
 
-if [ -n "$testing" ]; then
-    if [ -x "$(git branch --list | grep '*' | grep 'dev')" ]; then echo "Not on dev branch, exiting."; exit 1; fi
-    echo "Testing... setting baseUrl to dev branch..."
-    az group create --name $resource_group --location $location
-    az group deployment create \
-     --resource-group $resource_group \
-     --template-file mainTemplate.json \
-     --parameters @mainTemplateParameters.json \
-     --parameters '{"baseUrl": {"value": "https://raw.githubusercontent.com/DSPN/azure-resource-manager-dse/dev"}}' \
-     --verbose
-else
-    az group create --name $resource_group --location $location
-    az group deployment create \
-     --resource-group $resource_group \
-     --template-file mainTemplate.json \
-     --parameters @mainTemplateParameters.json \
-     --verbose
-fi
+az group create --name $resource_group --location $location
+az group deployment create \
+ --resource-group $resource_group \
+ --template-file mainTemplate.json \
+ --parameters @mainTemplateParameters.json \
+ --parameters '{"baseUrl": {"value": "https://raw.githubusercontent.com/DSPN/azure-resource-manager-dse/workshop"}}' \
+ --verbose
