d52680d2efddb75519e18641edec8b1c505a8202
Collin Poczatek <collin.poczatek@gmail.com>
2018-05-10 11:24:29 -0700

    clean, set baseUrl

--- singledc/deploy.sh
+++ singledc/deploy.sh
@@ -4,19 +4,18 @@ resource_group='dse'
 location='eastus'
 usage="---------------------------------------------------
 Usage:
-deploy.sh [-h] [-g resource-group] [-l location] [-t]
+deploy.sh [-h] [-g resource-group] [-l location]
 
 Options:
 
  -h                 : display this message and exit
  -g resource-group  : name of resource group to create, default 'dse'
  -l location        : location for resource group, default 'eastus'
- -t                 : testing flag, sets baseUrl to dev branch
 
 ---------------------------------------------------"
 
 
-while getopts 'hg:l:t' opt; do
+while getopts 'hg:l:' opt; do
   case $opt in
     h) echo -e "$usage"
        exit 1
@@ -24,8 +23,6 @@ while getopts 'hg:l:t' opt; do
     g) resource_group="$OPTARG"
     ;;
     l) location="$OPTARG"
-    ;;
-    t) testing="true"
     ;;
     \?) echo "Invalid option -$OPTARG" >&2
         exit 1
@@ -42,21 +39,11 @@ fi
 echo "CLI v2 'az' command found"
 echo "Using values: resource_group=$resource_group location=$location"
 
-if [ -n "$testing" ]; then
-    if [ -x "$(git branch --list | grep '*' | grep 'dev')" ]; then echo "Not on dev branch, exiting."; exit 1; fi
-    echo "Testing... setting baseUrl to dev branch..."
-    az group create --name $resource_group --location $location
-    az group deployment create \
-     --resource-group $resource_group \
-     --template-file mainTemplate.json \
-     --parameters @mainTemplateParameters.json \
-     --parameters '{"baseUrl": {"value": "https://raw.githubusercontent.com/DSPN/azure-resource-manager-dse/dev"}}' \
-     --verbose
-else
-    az group create --name $resource_group --location $location
-    az group deployment create \
-     --resource-group $resource_group \
-     --template-file mainTemplate.json \
-     --parameters @mainTemplateParameters.json \
-     --verbose
-fi
+eco "WARNING: baseUrl hardcoded to rr branch"
+
+az group create --name $resource_group --location $location
+az group deployment create \
+ --resource-group $resource_group \
+ --template-file mainTemplate.json \
+ --parameters @mainTemplateParameters.json \
+ --verbose
