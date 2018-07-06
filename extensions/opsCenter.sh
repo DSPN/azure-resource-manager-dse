d80eb0253ad96198a26ac1c5b04c68f0d9e408a2
Collin Poczatek <collin.poczatek@gmail.com>
2018-07-06 11:23:37 -0700

    skip alterKeyspaces

--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -73,5 +73,6 @@ echo repopw XXXXXX
 --opscpw $opscpw
 # set keyspaces to NetworkTopology / RF 3
 sleep 30s
-./lcm/alterKeyspaces.py \
---opscpw $opscpw \
+echo "skipping alterKeyspaces"
+#./lcm/alterKeyspaces.py \
+#--opscpw $opscpw \
