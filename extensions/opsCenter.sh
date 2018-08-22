ac60fb93d7757e0c44dfe19e29e53fd4e1ccba84
Collin Poczatek <collin.poczatek@gmail.com>
2018-08-22 14:31:38 -0700

    merge in from master

--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -5,6 +5,7 @@ password=$2
 opscpw=$3
 dbpasswd=$4
 nodecount=$5
+ver=$6
 
 echo "Input to opsCenter.sh is:"
 echo username $username
@@ -19,59 +20,62 @@ cluster_name="mycluster"
 repouser='datastax@microsoft.com'
 repopw='3A7vadPHbNT'
 
-release="workshop"
+release="7.1.0"
 wget https://github.com/DSPN/install-datastax-ubuntu/archive/$release.tar.gz
 tar -xvf $release.tar.gz
 
 cd install-datastax-ubuntu-$release/bin
-# install extra packages
+# install extra packages, openjdk
 ./os/extra_packages.sh
+./os/install_java.sh -o
 
 # Overide OpsC install default version if needed
-export OPSC_VERSION='6.5.0'
-ver='6.0.0'
-
-./os/install_java.sh
+export OPSC_VERSION='6.5.2'
 
 #install opsc
 ./opscenter/install.sh 'azure'
-./opscenter/start.sh
 # Turn on https, set pw for opsc user admin
-# Comment out for workshop
-#./opscenter/set_opsc_pw_https.sh $opscpw
+./opscenter/set_opsc_pw_https.sh $opscpw
 sleep 1m
 
-# This config profile turns off auth, and overrides any other config settings
-# created by other args except dsever
-config="{ \"datastax-version\": \"6.0.0\", \"name\": \"test\", \"json\": { \"cassandra-yaml\": { \"authorizer\": \"AllowAllAuthorizer\", \"saved_caches_directory\": \"/data/cassandra/saved_caches\", \"data_file_directories\": [ \"/data/cassandra/data\" ], \"num_tokens\": 32, \"authenticator\": \"AllowAllAuthenticator\", \"endpoint_snitch\": \"org.apache.cassandra.locator.GossipingPropertyFileSnitch\", \"commitlog_directory\": \"/data/cassandra/commitlog\" }, \"dse-yaml\": { \"authorization_options\": { \"enabled\": true }, \"authentication_options\": { \"enabled\": true }, \"resource_manager_options\": { \"worker_options\": { \"workpools\": [ { \"memory\": \"0.25\", \"cores\": \"0.25\", \"name\": \"alwayson_sql\" } ] } }, \"alwayson_sql_options\": { \"enabled\": true } } }}"
-
 echo "Calling setupCluster.py with the settings:"
-echo opsc_ip 127.0.0.1s
+echo opsc_ip 127.0.0.1
 echo cluster_name $cluster_name
 echo username $username
 echo password XXXXXX
 echo repouser $repouser
 echo repopw XXXXXX
-echo config $config
 
+# add --nojava to setupCluster call below,
+# java must be installed on nodes separate from LCM
 ./lcm/setupCluster.py \
+--opscpw $opscpw \
 --clustername $cluster_name \
 --repouser $repouser \
 --repopw $repopw \
+--dsever  $ver \
 --user $username \
 --password $password \
 --dbpasswd $dbpasswd \
---config "$config"
+--datapath "/data/cassandra" \
+--nojava \
+--verbose
 
 # trigger install
 ./lcm/triggerInstall.py \
+--opscpw $opscpw \
 --clustername $cluster_name \
---clustersize $nodecount
-
+--clustersize $nodecount \
+--pause 10 \
+--trys 400
 
 # Block execution while waiting for jobs to
 # exit RUNNING/PENDING status
-./lcm/waitForJobs.py
+./lcm/waitForJobs.py \
+--opscpw $opscpw
 # set keyspaces to NetworkTopology / RF 3
-sleep 30s
-./lcm/alterKeyspaces.py
+
+echo "Backgrounding call to alterKeyspaces.py, writing ouput to repair.log... "
+nohup ./lcm/alterKeyspaces.py \
+--opscpw $opscpw \
+--delay 60 >> ../../repair.log &
