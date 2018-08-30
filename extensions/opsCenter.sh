780c7c3262bbf7c24569efd6515e71420a41d0dc
Collin Poczatek <collin.poczatek@gmail.com>
2018-08-30 12:30:26 -0700

    fix merge

--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -34,9 +34,16 @@ export OPSC_VERSION='6.5.2'
 
 #install opsc
 ./opscenter/install.sh 'azure'
+./opscenter/start.sh
 # Turn on https, set pw for opsc user admin
-./opscenter/set_opsc_pw_https.sh $opscpw
+# Comment out for workshop
+#./opscenter/set_opsc_pw_https.sh $opscpw
 sleep 1m
+
+# This config profile turns off auth, and overrides any other config settings
+# created by other args except dsever
+config="{ \"datastax-version\": \"6.0.0\", \"name\": \"test\", \"json\": { \"cassandra-yaml\": { \"authorizer\": \"AllowAllAuthorizer\", \"saved_caches_directory\": \"/data/cassandra/saved_caches\", \"data_file_directories\": [ \"/data/cassandra/data\" ], \"num_tokens\": 32, \"authenticator\": \"AllowAllAuthenticator\", \"endpoint_snitch\": \"org.apache.cassandra.locator.GossipingPropertyFileSnitch\", \"commitlog_directory\": \"/data/cassandra/commitlog\" }, \"dse-yaml\": { \"authorization_options\": { \"enabled\": true }, \"authentication_options\": { \"enabled\": true }, \"resource_manager_options\": { \"worker_options\": { \"workpools\": [ { \"memory\": \"0.25\", \"cores\": \"0.25\", \"name\": \"alwayson_sql\" } ] } }, \"alwayson_sql_options\": { \"enabled\": true } } }}"
+
 
 echo "Calling setupCluster.py with the settings:"
 echo opsc_ip 127.0.0.1
@@ -49,21 +56,16 @@ echo repopw XXXXXX
 # add --nojava to setupCluster call below,
 # java must be installed on nodes separate from LCM
 ./lcm/setupCluster.py \
---opscpw $opscpw \
 --clustername $cluster_name \
 --repouser $repouser \
 --repopw $repopw \
---dsever  $ver \
+--config "$config" \
 --user $username \
 --password $password \
---dbpasswd $dbpasswd \
---datapath "/data/cassandra" \
---nojava \
 --verbose
 
 # trigger install
 ./lcm/triggerInstall.py \
---opscpw $opscpw \
 --clustername $cluster_name \
 --clustersize $nodecount \
 --pause 10 \
@@ -71,11 +73,7 @@ echo repopw XXXXXX
 
 # Block execution while waiting for jobs to
 # exit RUNNING/PENDING status
-./lcm/waitForJobs.py \
---opscpw $opscpw
-# set keyspaces to NetworkTopology / RF 3
+./lcm/waitForJobs.py
 
-echo "Backgrounding call to alterKeyspaces.py, writing ouput to repair.log... "
-nohup ./lcm/alterKeyspaces.py \
---opscpw $opscpw \
---delay 60 >> ../../repair.log &
+# set keyspaces to NetworkTopology / RF 3
+./lcm/alterKeyspaces.py 
