bdbd21c7292869dac344db531b0946f02b833f8d
Collin Poczatek <collin.poczatek@gmail.com>
2018-09-06 14:51:14 -0700

    usermod, install az/azcopy

--- extensions/node.sh
+++ extensions/node.sh
@@ -32,6 +32,7 @@ mkdir -p /data/cassandra/data
 mkdir -p /data/cassandra/commitlog
 mkdir -p /data/cassandra/saved_caches
 useradd cassandra
+usermod -d /var/lib/cassandra -s /bin/false cassandra
 chown -R cassandra:cassandra /data/cassandra
 
 release="7.1.0"
@@ -42,6 +43,20 @@ cd install-datastax-ubuntu-$release/bin/
 # install extra packages, openjdk
 ./os/extra_packages.sh
 ./os/install_java.sh -o
+# install az/azcopy
+# az repo
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+# azcopy repo
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod/ xenial main" > azure.list
+cp ./azure.list /etc/apt/sources.list.d/
+apt-key adv --keyserver packages.microsoft.com --recv-keys EB3E94ADBE1229CF
+#3 install
+apt-get install apt-transport-https
+apt-get update && sudo apt-get -y install azure-cli azcopy
+
 
 # grabbing metadata after extra_packages.sh to ensure we have jq
 cluster_name="mycluster"
