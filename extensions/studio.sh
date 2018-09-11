95d73ddc7e5f4e532ff8e3557edf8d6cf8468262
Collin Poczatek <collin.poczatek@gmail.com>
2018-09-11 16:51:32 -0700

    add setup notebook

--- extensions/studio.sh
+++ extensions/studio.sh
@@ -13,6 +13,9 @@ sed -i -e 's/httpBindAddress: localhost/httpBindAddress: 0.0.0.0/g' ./datastax-s
 # DONT setup connection
 echo "Remove sparksql example..."
 rm ./datastax-studio-$sversion/examples/notebooks/3a0a2fc4-d7d7-40ba-9438-8d672da3c1d5
+echo "Add instruction notebook..."
+curl -sS -O https://dsetestdrivestor.blob.core.windows.net/studio/32714686-1bbd-45cd-bed4-c95de5c242d0
+mv 32714686-1bbd-45cd-bed4-c95de5c242d0 ./datastax-studio-$sversion/examples/notebooks/
 # make not root owned
 chown -R $user:$user ./datastax-studio-$sversion
 
