00ba4a364a408b72659fb13d8dd109ae91d0a737
Collin Poczatek <collin.poczatek@gmail.com>
2018-11-02 14:15:33 -0700

    point at master of install scripts

--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -23,7 +23,7 @@ cluster_name="mycluster"
 repouser='datastax@microsoft.com'
 repopw='3A7vadPHbNT'
 
-release="7.1.0"
+release="master"
 wget https://github.com/DSPN/install-datastax-ubuntu/archive/$release.tar.gz
 tar -xvf $release.tar.gz
 
